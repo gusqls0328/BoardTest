8c110e7262ed7e24349f10ab8e00e96a2f19d322
gusqls0328 <gusqls0328@naver.com>
2019-10-23 17:55:46 +0900

    수정2

--- src/main/java/com/mycompany/web/dao/Login1Dao01.java
+++ src/main/java/com/mycompany/web/dao/Login1Dao01.java
@@ -1,5 +1,10 @@
 package com.mycompany.web.dao;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Login1Dao01 {
 
+	
+	
 }
--- src/main/java/com/mycompany/web/service/Login1Service01.java
+++ src/main/java/com/mycompany/web/service/Login1Service01.java
@@ -2,10 +2,14 @@ package com.mycompany.web.service;
 
 import org.springframework.stereotype.Service;
 
+import com.mycompany.web.dto.LoginMember;
+
 @Service
 public class Login1Service01 {
 
 	public LoginResult login(String mid, String mpassword) {
+		
+		LoginMember loginMember = login1Dao01.selectloginMember(mid);
 		
 		
 		return null;
