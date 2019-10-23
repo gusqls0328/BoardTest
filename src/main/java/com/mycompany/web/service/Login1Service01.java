812c481d85940a0b53bc335367ba9b60d63d5f9e
gusqls0328 <gusqls0328@naver.com>
2019-10-23 17:56:56 +0900

    수정3

--- src/main/java/com/mycompany/web/service/Login1Service01.java
+++ src/main/java/com/mycompany/web/service/Login1Service01.java
@@ -6,7 +6,10 @@ import com.mycompany.web.dto.LoginMember;
 
 @Service
 public class Login1Service01 {
-
+	
+	
+	@
+	
 	public LoginResult login(String mid, String mpassword) {
 		
 		LoginMember loginMember = login1Dao01.selectloginMember(mid);
